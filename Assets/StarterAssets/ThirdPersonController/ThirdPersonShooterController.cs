2ecfa7b4f43912df6fe30a0bcc798fcab9aea5a7
Slavic Culev <slavicculev@Slavics-MacBook-Air.local>
2024-08-02 18:26:16 +0300

    added TPS assets, crosshair, aiming

---
+++ Assets/StarterAssets/ThirdPersonController/ThirdPersonShooterController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using StarterAssets;
+using UnityEngine.InputSystem;
+
+public class ThirdPersonShooterController : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera aimVirtualCamera;
+    [SerializeField] private float normalSensitivity;
+    [SerializeField] private float aimSensitivity;
+    [SerializeField] private LayerMask aimColliderLayerMask = new LayerMask();
+    [SerializeField] private Transform debugTransform;
+
+    private ThirdPersonController _thirdPersonController;
+    private StarterAssetsInputs _starterAssetsInputs;
+
+    private void Awake()
+    {
+        _thirdPersonController = GetComponent<ThirdPersonController>();
+        _starterAssetsInputs = GetComponent<StarterAssetsInputs>();
+    }
+
+    private void Update()
+    {
+        if (_starterAssetsInputs.aim)
+        {
+            aimVirtualCamera.gameObject.SetActive(true);
+            _thirdPersonController.SetSensitivity(aimSensitivity);
+        }
+        else
+        {
+            aimVirtualCamera.gameObject.SetActive(false);
+            _thirdPersonController.SetSensitivity(normalSensitivity);
+        }
+
+        Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask))
+        {
+            debugTransform.position = raycastHit.point;
+        }
+
+    }
+}
